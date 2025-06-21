4f863643620f39febefa43b6af88e2eef5dcde96
juliaprzezdzik <113039967+juliaprzezdzik@users.noreply.github.com>
2025-06-21 19:05:06 +0200

    Update bankruptcy.py

--- bankruptcy/bankruptcy.py
+++ bankruptcy/bankruptcy.py
@@ -80,40 +80,64 @@ plt.show()
 
 import shap
 
+
 background_data_resampled = shap.sample(X_train_resampled, 100)
 explainer = shap.KernelExplainer(model_full.predict, background_data_resampled)
-shap_values = explainer.shap_values(X_test_scaled)
-print("\nWykres SHAP:")
-shap.summary_plot(shap_values[0], X_test_scaled, feature_names=X.columns)
+X_test_sample = shap.sample(X_test_scaled, 200) 
+shap_values = explainer.shap_values(X_test_sample)
+shap.summary_plot(shap_values, X_test_sample, feature_names=X.columns)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+shap_values_flat = np.abs(shap_values[1]).flatten()
+print(f"Liczba cech (kolumn): {len(X.columns)}")
+print(f"Liczba obliczonych ważności: {len(shap_values_flat)}")
+feature_importance_df = pd.DataFrame({
+    'Cecha': X.columns,
+    'Ważność SHAP': shap_values_flat
+})
+
+feature_importance_df = feature_importance_df.sort_values(by='Ważność SHAP', ascending=False)
+plt.figure(figsize=(10, 8))
+plt.title('Najważniejsze cechy wg sredniej wartosci Shap')
+plt.barh(
+    feature_importance_df['Cecha'][:15][::-1], 
+    feature_importance_df['Ważność SHAP'][:15][::-1]
+)
+plt.xlabel("Średnia absolutna wartość SHAP (wpływ na predykcję modelu)")
+plt.grid(axis='x')
+plt.show()
 
 ### redukcja cech i ponowny trening
-mean_abs_shap = np.abs(shap_values[0]).mean(0)
-feature_importance = pd.DataFrame(list(zip(X.columns, mean_abs_shap)), columns=['Cecha', 'Ważność SHAP'])
-feature_importance = feature_importance.sort_values(by='Ważność SHAP', ascending=True)
-num_features_to_drop = 15
-features_to_drop = feature_importance.head(num_features_to_drop)['Cecha'].tolist()
-print(f"\nCechy do usuniecia: {features_to_drop}")
 
+num_features_to_drop = 15
+features_to_drop = feature_importance_df.tail(num_features_to_drop)['Cecha'].tolist()
+print(f"\nCechy do usunięcia: {features_to_drop}")
 X_train_reduced = X_train.drop(columns=features_to_drop)
 X_test_reduced = X_test.drop(columns=features_to_drop)
-
-X_train_reduced_imputed = imputer.transform(X_train_reduced)
-X_test_reduced_imputed = imputer.transform(X_test_reduced)
-
-X_train_reduced_scaled = scaler.transform(X_train_reduced_imputed)
-X_test_reduced_scaled = scaler.transform(X_test_reduced_imputed)
-X_train_reduced_resampled, y_train_reduced_resampled = smote.fit_resample(X_train_reduced_scaled, y_train)
-
-model_reduced = Sequential([Dense(64, activation='relu', input_shape=(X_train_reduced_resampled.shape[1],)),
+imputer_reduced = SimpleImputer(strategy='median')
+X_train_reduced_imputed = imputer_reduced.fit_transform(X_train_reduced)
+X_test_reduced_imputed = imputer_reduced.transform(X_test_reduced)
+scaler_reduced = StandardScaler()
+X_train_reduced_scaled = scaler_reduced.fit_transform(X_train_reduced_imputed)
+X_test_reduced_scaled = scaler_reduced.transform(X_test_reduced_imputed)
+smote_reduced = SMOTE(random_state=42)
+X_train_reduced_resampled, y_train_reduced_resampled = smote_reduced.fit_resample(X_train_reduced_scaled, y_train)
+model_reduced = Sequential([ Dense(64, activation='relu', input_shape=(X_train_reduced_resampled.shape[1],)),
 Dropout(0.5),Dense(32, activation='relu'),Dropout(0.5),Dense(1, activation='sigmoid')])
 
 model_reduced.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Precision(name='precision'), Recall(name='recall')])
+
 model_reduced.summary()
+history_reduced = model_reduced.fit(X_train_reduced_resampled, y_train_reduced_resampled,
+epochs=50,batch_size=256,validation_data=(X_test_reduced_scaled, y_test),verbose=0 )
 
-history_reduced = model_reduced.fit(X_train_reduced_resampled, y_train_reduced_resampled,epochs=50,batch_size=256,validation_data=(X_test_reduced_scaled, y_test),verbose=0)
 
-print("\n Porownanie ")
-print("\nModel z pelnym zbiorem cech:")
+### porownanie obu modeli 
+
+print("\nModel z pełnym zbiorem cech:")
 results_full = model_full.evaluate(X_test_scaled, y_test, verbose=0)
 for name, value in zip(model_full.metrics_names, results_full):
     print(f"{name}: {value:.4f}")
